0ee8ce3aeccf9f5ff9f719018c81f7cb999ae2b3
Garrett <garrettjames55@gmail.com>
2024-09-15 12:16:15 -0400

    Passport configured

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import { ApolloServer } from '@apollo/server'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { buildContext } from 'graphql-passport'
+import { configurePassport } from './passport/passport.config.js'
 import { connectDB } from './db/connectDB.js'
+import connectMongo from 'connect-mongodb-session'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
@@ -8,12 +11,44 @@ import { expressMiddleware } from '@apollo/server/express4'
 import http from 'http'
 import mergedResolvers from './resolvers/index.js'
 import mergedTypeDefs from './typeDefs/index.js'
+import passport from 'passport'
+import session from 'express-session'
 
 dotenv.config()
+
+configurePassport()
 
 const app = express()
 
 const httpServer = http.createServer(app)
+
+// Create a new MongoDBStore instance
+const MongoDBStore = connectMongo(session)
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: 'sessions'
+})
+
+// Catch errors
+store.on('error', err => console.error(err))
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // don't save a new session if it hasn't been modified
+    saveUninitialized: false, // don't create a session until something is stored
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      httpOnly: true // the cookie is only accessible by the server
+    },
+    store
+  })
+)
+
+// Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.initialize())
+app.use(passport.session())
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -26,12 +61,15 @@ await server.start()
 
 app.use(
   '/',
-  cors(),
+  cors({
+    origin: 'http://localhost:3000', // allow to server to accept request from different origin
+    credentials: true // allow session cookie from browser to pass through
+  }),
   express.json(),
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req })
+    context: async ({ req, res }) => buildContext({ req, res }) // buildContext is a function that returns an object with the request and response objects
   })
 )
 
---
+++ backend/passport/passport.config.js
@@ -0,0 +1,41 @@
+import { GraphQLLocalStrategy } from 'graphql-passport'
+import User from '../models/user.model.js'
+import bcrypt from 'bcrypt'
+import passport from 'passport'
+
+export const configurePassport = async () => {
+  passport.serializeUser((user, done) => {
+    console.log('Serializing user')
+    done(null, user.id)
+  }) // serialize user to store in session
+
+  passport.deserializeUser(async (id, done) => {
+    console.log('Deserializing user')
+    try {
+      const user = await User.findById(id) // find user by id in database
+      done(null, user)
+    } catch (err) {
+      done(err, null)
+    }
+  }) // deserialize user from session
+
+  passport.use(
+    new GraphQLLocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username })
+        if (!user) {
+          throw new Error('Invalid username or password')
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password) // compare password with hashed password
+
+        if (!isPasswordValid) {
+          throw new Error('Invalid username or password')
+        }
+
+        return done(null, user)
+      } catch (err) {
+        return done(err, null)
+      }
+    })
+  )
+}
